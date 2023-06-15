efefb10d4ad3c16bc14365909b8b7bc9cc88397c
Aldo Santos <aldoacesso@gmail.com>
2023-06-14 23:31:25 -0300

    add Front End e Componente Vídeo

---
+++ src/pages/Video.js
@@ -0,0 +1,35 @@
+import React, { useRef, useState } from 'react'
+import "./video.css"
+
+function Video() {
+
+    const videoRef = useRef(null)
+    const [play, setplay] = useState(false)
+
+    function handdleStart() {
+
+        if(play) {
+            videoRef.current.pause()
+            setplay(false)
+        } else {
+            videoRef.current.play()
+            setplay(true)
+        }
+    }
+
+  return (
+    <div className='video'>
+        <video 
+            className='video__player'
+            ref={videoRef}
+            onClick={handdleStart}
+            loop
+            src='https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z'
+        >
+
+        </video>
+    </div>
+  );
+}
+
+export default Video
