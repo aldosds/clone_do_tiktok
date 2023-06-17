22abc5568db86a04642927bc342d001086052c6a
Aldo Santos <aldoacesso@gmail.com>
2023-06-17 00:33:17 -0300

    add Componente Sidebar e preparação do UI para receber DB

--- src/App.js
+++ src/App.js
@@ -6,10 +6,24 @@ function App() {
   return (
     <div className="App">
       <div className='app__videos'>
-        <Video />
-        <Video />
-        <Video />
-        <Video />
+        <Video 
+          likes={111}
+          messages={222}
+          shares={333}
+          name="Aldo"
+          description="Brecker o goleiro"
+          music="musica épica"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
+        />
+        <Video 
+          likes={444}
+          messages={555}
+          shares={666}
+          name="Santos"
+          description="Que bonito gatito"
+          music="musica engraçada"
+          url="https://firebasestorage.googleapis.com/v0/b/video-2db61.appspot.com/o/X2Download.app-Que%20Bonito%20Gatito%20%23Shorts%20%23top-(720p60).mp4?alt=media&token=538b050d-b480-4995-a3a8-2ba5dcecc808"
+        />
       </div>
     </div>
   );
